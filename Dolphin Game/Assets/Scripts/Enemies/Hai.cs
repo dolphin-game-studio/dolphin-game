d1e38fde6dae0544df253a3cf8c8c94cf34128e1
Alexander Johr <alexander.johr@gmail.com>
2019-08-15 02:40:59 +0200

    Made that bubble doesn't move sharks and that it gets destroyed on collision

--- Dolphin Game/Assets/Scripts/Enemies/Hai.cs
+++ Dolphin Game/Assets/Scripts/Enemies/Hai.cs
@@ -14,7 +14,7 @@ public class Hai : MonoBehaviour
 
     [Range(0, 360)]
     public float viewAngle;
-    
+
     #region View Radius
 
     [Range(1, 100)]
@@ -157,7 +157,7 @@ public class Hai : MonoBehaviour
 
     #region Ranks
     [Range(1, 3)]
-    public int rank = 1; 
+    public int rank = 1;
 
     public int Rank
     {
@@ -196,7 +196,7 @@ public class Hai : MonoBehaviour
 
     public bool IsNotDistracted { get => !IsDistracted; set => IsDistracted = !value; }
     #endregion
-     
+
     void Start()
     {
         HaiPatrouille = GetComponent<HaiPatrouille>();
@@ -366,9 +366,12 @@ public class Hai : MonoBehaviour
     #region Look At Player Character
     private void LookAtPlayerCharacter(GameObject visiblePlayer)
     {
-        Vector3 dirToTarget = (visiblePlayer.transform.position - transform.position).normalized;
-        transform.forward = dirToTarget;
-        currentViewPanRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y > 180 ? -90 : 90, transform.eulerAngles.z);
+        if (visiblePlayer != null)
+        {
+            Vector3 dirToTarget = (visiblePlayer.transform.position - transform.position).normalized;
+            transform.forward = dirToTarget;
+            currentViewPanRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y > 180 ? -90 : 90, transform.eulerAngles.z);
+        }
     }
     #endregion
 
@@ -609,7 +612,7 @@ public class Hai : MonoBehaviour
     #endregion
 
     #region Collision with Player
-    
+
     private CapsuleCollider Collider;
 
     int playerCharacterLayer;
